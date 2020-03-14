0d8ae4c93c5c3aaa04af6b7cab6c6e0729300d28
nicola <nicola.farina@pangaeformazione.org>
2020-03-14 20:38:39 +0100

    fit esponenziale con decadimento lineare

--- webapp/funzionifit.R
+++ webapp/funzionifit.R
@@ -58,11 +58,15 @@ predictNextDays<-function(dati,modello,nahead=3, all=FALSE) {
 		newdata <- data.frame(data = c(dati$data, max(dati$data)+seq_len(nahead))  )
 	} else newdata<-data.frame(data = max(dati$data)+seq_len(nahead))
 
-	predizioni<-exp(predict(modello,newdata,interval="confidence"))
+	predizioni<-try(exp(predict(modello,newdata,interval="confidence")))
+	if (inherits(predizioni,"try-error")) {
+		warning("Can't make predictions")
+		tmp<-rep(NA_real_,nahead)
+		return(data.frame(Attesi=tmp,LowerRange=tmp,UpperRange=tmp))
+	}
 	colnames(predizioni)<-c("Attesi","LowerRange","UpperRange")
 	newdata<-cbind(newdata,predizioni)
 	return(newdata)
-	newdata
 }
 
 loglinmodel2<-function(dati, var="totale_casi", rangepesi=c(0,1)) {
@@ -77,3 +81,47 @@ loglinmodel2<-function(dati, var="totale_casi", rangepesi=c(0,1)) {
 	#abline(fit2,col="green")
 	fit
 }
+
+loglinmodel3<-function(dati, var="totale_casi", rangepesi=c(0,1), quadratico = FALSE) {
+	regione <- copy(dati)
+	setnames(regione, old=var, new='var2fit')
+	regione$logcasi <- log(regione$var2fit)
+	regione<-regione[is.finite(regione$logcasi),]
+	pesi<-seq(rangepesi[1],rangepesi[2],length.out=nrow(regione))
+	if (quadratico) {
+		regione$dataind<-as.numeric(regione$data-min(regione$data))+1
+		regione$data2<-as.numeric(regione$dataind)^2
+		fit<-lm(logcasi~data+data2,regione,weights=pesi)
+		attr(fit,"quadratico")<-TRUE
+		attr(fit,"mindata")<-min(regione$data)
+	} else {
+		fit<-lm(logcasi~data,regione,weights=pesi)
+	}
+	setnames(regione, old='var2fit', new=var)
+	#plot(regione$data,regione$logcasi,col="red",lwd=2,ty="b")
+	#abline(fit2,col="green")
+	fit
+}
+
+
+ismodelloQuadratico<-function(modello) {
+	tmp<-attr(modello,"quadratico")
+	!is.null(tmp) && tmp
+}
+
+
+predictNextDays<-function(dati,modello,nahead=3, all=FALSE) {
+	if(all==TRUE){
+		newdata <- data.frame(data = c(dati$data, max(dati$data)+seq_len(nahead))  )
+	} else newdata<-data.frame(data = max(dati$data)+seq_len(nahead))
+	if (ismodelloQuadratico(modello)) {
+		newdata$dataind<-as.numeric(newdata$data-attr(modello,"mindata"))+1
+		newdata$data2<-newdata$dataind^2
+	}
+	predizioni<-exp(predict(modello,newdata,interval="confidence"))
+	colnames(predizioni)<-c("Attesi","LowerRange","UpperRange")
+	newdata<-cbind(newdata,predizioni)
+	return(newdata)
+	newdata
+}
+
